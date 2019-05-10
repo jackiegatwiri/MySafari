be41977f679a1a66aea87da6b2503c1a1f0cd9ae
jackiegatwiri <jaciegatz@gmail.com>
2019-05-10 16:17:13 +0300

    Create java class WelcomeActivity and its layout. This will be my launcher page

--- app/src/main/java/com/jacky/mysafari/WelcomeActivity.java
+++ app/src/main/java/com/jacky/mysafari/WelcomeActivity.java
@@ -1,13 +1,14 @@
 package com.jacky.mysafari;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
