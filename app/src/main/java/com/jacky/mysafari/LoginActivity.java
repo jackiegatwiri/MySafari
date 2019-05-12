89f8e1f08bbf59c1e1a201c1b716c35dce372ae1
jackiegatwiri <jaciegatz@gmail.com>
2019-05-12 18:39:29 +0300

    add intent to move from one activity to another. Add intent into the login class to move from login page to signup page and from signup page to login page

--- app/src/main/java/com/jacky/mysafari/LoginActivity.java
+++ app/src/main/java/com/jacky/mysafari/LoginActivity.java
@@ -2,13 +2,24 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-}
+
+     public void btn_signup(View view) {
+
+        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+     }
+
+     public void btn_main(View view) {
+         startActivity(new Intent(getApplicationContext(), MainActivity.class));
+     }
+ }
