07badb0bc66a333c10e08504837c298fc3f349ce
jackiegatwiri <jaciegatz@gmail.com>
2019-05-12 15:10:43 +0300

    add a logo to my welcome page

--- app/src/main/java/com/jacky/mysafari/WelcomeActivity.java
+++ app/src/main/java/com/jacky/mysafari/WelcomeActivity.java
@@ -2,13 +2,25 @@ package com.jacky.mysafari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
