ac70908730807213580a24ca3e198321b924eacc
jackiegatwiri <jaciegatz@gmail.com>
2019-05-14 19:19:21 +0300

    changed signup nd login activity

--- app/src/main/java/com/jacky/mysafari/SignUpActivity.java
+++ app/src/main/java/com/jacky/mysafari/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.jacky.mysafari;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Patterns;
@@ -11,116 +14,80 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class SignUpActivity extends AppCompatActivity {
-    private static final String TAG = "SignupActivity";
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.name) TextView mname;
+    @BindView(R.id.email) TextView memail;
+    @BindView(R.id.password) TextView mpassword;
+    @BindView(R.id.confirmPassword) TextView mconfirmPassword;
+    @BindView(R.id.signup) Button msignup;
+    @BindView(R.id.login) TextView mlogin;
 
-    @BindView(R.id.input_name) EditText _nameText;
-    @BindView(R.id.input_email) EditText _emailText;
-    @BindView(R.id.input_password) EditText _passwordText;
-    @BindView(R.id.btn_signup) Button _signupButton;
-    @BindView(R.id.link_login) TextView _loginLink;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
 
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signup();
-            }
-        });
-
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });
+        msignup.setOnClickListener(this);
+        mlogin.setOnClickListener(this);
     }
+    public boolean validation(){
+        boolean valid = false;
 
-    public void signup() {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
+        if(mname.getText().toString().isEmpty()){
+            mname.setError("Invalid name");
         }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
-
-    public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _signupButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (name == "" || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            _nameText.setError(null);
+        else if(memail.getText().toString().isEmpty() || !(Patterns.EMAIL_ADDRESS.matcher(memail.getText().toString()).matches())){
+            memail.setError("Invalid email address");
         }
-
-        if (email == "" || !(Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
+        else if(mpassword.getText().toString().isEmpty() || mpassword.getText().toString().length() < 8 ){   //  !(isValidPassword(mpassword.getText().toString().trim()))  ){
+            mpassword.setError("Use at least one uppercase letter, lowercase letter, number, special characters and");
         }
-
-        if (password == "" || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
+        else if(!mconfirmPassword.getText().toString().equals(mpassword.getText().toString())){
+            mconfirmPassword.setError("Password does not match");
         }
+        else
+            {
+            valid = true;
 
+        }
         return valid;
     }
+
+//    public static boolean isValidPassword(final String password) { //password matcher
+//
+//        Pattern pattern;
+//        Matcher matcher;
+//        final String PASSWORD_PATTERN = "/^(?=.*\\d)(?=.*[A-Z])([@$%&#])[0-9a-zA-Z]{4,}$/";
+//        pattern = Pattern.compile(PASSWORD_PATTERN);
+//        matcher = pattern.matcher(password);
+//
+//        return matcher.matches();
+//
+//    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == msignup){
+            if (validation()) {
+                Toast.makeText(this, "Sign Up Successfully", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (v == mlogin){
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
+
+
